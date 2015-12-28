b7ccde97475cf4503388f900d41721fb225ced03
Massimo Bonanni <massimo.bonanni@tiscali.it>
2015-12-28 19:53:54 +0100

    Change visualization in Face Landmark demo

---
+++ WPFCore/PXCMImageExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WPFCore
+{
+    public static class PXCMImageExtensions
+    {
+        public static WriteableBitmap GetImage(this PXCMImage image)
+        {
+            PXCMImage.ImageData imageData = null;
+            WriteableBitmap returnImage = null;
+            int width = 0;
+            int height = 0;
+            if (image.AcquireAccess(PXCMImage.Access.ACCESS_READ,
+                                   PXCMImage.PixelFormat.PIXEL_FORMAT_RGB32,
+                                   out imageData).IsSuccessful())
+            {
+                width = Convert.ToInt32(imageData.pitches[0] / 4);
+                height = image.info.height;
+                returnImage = imageData.ToWritableBitmap(width, height, 96, 96);
+                image.ReleaseAccess(imageData);
+            }
+            return returnImage;
+        }
+    }
+}
