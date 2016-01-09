0e8c536537377152d050516828151bd90718a77f
Massimo Bonanni <massimo.bonanni@tiscali.it>
2016-01-09 19:41:12 +0100

    Face Expression sample added

---
+++ FaceExpressionWPF/FaceModel.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FaceExpressionWPF
+{
+    public class FaceModel : INotifyPropertyChanged
+    {
+
+        private class ExpressionSelector
+        {
+            public PXCMFaceData.ExpressionsData.FaceExpression Expression { get; set; }
+            public int Threshold { get; set; }
+            public Action DetectedExpressionAction { get; set; }
+            public Action NotDetectedExpressionAction { get; set; }
+
+            public void CheckExpression(PXCMFaceData.ExpressionsData expressionData)
+            {
+                if (expressionData == null) return;
+                PXCMFaceData.ExpressionsData.FaceExpressionResult exprResult;
+                if (expressionData.QueryExpression(Expression, out exprResult))
+                {
+                    if (exprResult.intensity >= Threshold)
+                    {
+                        if (DetectedExpressionAction != null)
+                            DetectedExpressionAction.Invoke();
+                    }
+                    else
+                    {
+                        if (NotDetectedExpressionAction != null)
+                            NotDetectedExpressionAction.Invoke();
+                    }
+                }
+                else
+                {
+                    if (NotDetectedExpressionAction != null)
+                        NotDetectedExpressionAction.Invoke();
+                }
+            }
+        }
+
+        public FaceModel()
+        {
+            Expressions = new List<ExpressionSelector>();
+            Expressions.Add(new ExpressionSelector()
+            {
+                Expression = PXCMFaceData.ExpressionsData.FaceExpression.EXPRESSION_EYES_CLOSED_RIGHT,
+                Threshold = 80,
+                DetectedExpressionAction = () => IsEyeRightClosed = true,
+                NotDetectedExpressionAction = () => IsEyeRightClosed = false
+            });
+            Expressions.Add(new ExpressionSelector()
+            {
+                Expression = PXCMFaceData.ExpressionsData.FaceExpression.EXPRESSION_EYES_CLOSED_LEFT,
+                Threshold = 80,
+                DetectedExpressionAction = () => IsEyeLeftClosed = true,
+                NotDetectedExpressionAction = () => IsEyeLeftClosed = false
+            });
+            Expressions.Add(new ExpressionSelector()
+            {
+                Expression = PXCMFaceData.ExpressionsData.FaceExpression.EXPRESSION_TONGUE_OUT ,
+                Threshold = 100,
+                DetectedExpressionAction = () => IsTongueOut = true,
+                NotDetectedExpressionAction = () => IsTongueOut = false
+            });
+            //Expressions.Add(new ExpressionSelector()
+            //{
+            //    Expression = PXCMFaceData.ExpressionsData.FaceExpression.EXPRESSION_MOUTH_OPEN,
+            //    Threshold = 0,
+            //    DetectedExpressionAction = () =>
+            //    {
+            //        IsMouthNormal = false;
+            //        IsSmiling = false;
+            //        HasMouthOpen = true;
+            //    },
+            //    NotDetectedExpressionAction = () =>
+            //    {
+            //        HasMouthOpen = false;
+            //        IsMouthNormal = true;
+            //    }
+            //});
+            //Expressions.Add(new ExpressionSelector()
+            //{
+            //    Expression = PXCMFaceData.ExpressionsData.FaceExpression.EXPRESSION_SMILE,
+            //    Threshold = 50,
+            //    DetectedExpressionAction = () =>
+            //    {
+            //        IsMouthNormal = false;
+            //        IsSmiling = true;
+            //        HasMouthOpen = false;
+            //    },
+            //    NotDetectedExpressionAction = () =>
+            //    {
+            //        IsSmiling = false;
+            //        IsMouthNormal = true;
+            //    }
+            //});
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        private List<ExpressionSelector> Expressions;
+
+        private bool _isFaceVisible = false;
+        public bool IsFaceVisible
+        {
+            get { return _isFaceVisible; }
+            set
+            {
+                _isFaceVisible = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _isEyeRightClosed = false;
+        public bool IsEyeRightClosed
+        {
+            get { return _isEyeRightClosed; }
+            set
+            {
+                _isEyeRightClosed = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _isEyeLeftClosed = false;
+        public bool IsEyeLeftClosed
+        {
+            get { return _isEyeLeftClosed; }
+            set
+            {
+                _isEyeLeftClosed = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _hasMouthOpen = false;
+        public bool HasMouthOpen
+        {
+            get { return _hasMouthOpen; }
+            set
+            {
+                _hasMouthOpen = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _isSmiling = false;
+        public bool IsSmiling
+        {
+            get { return _isSmiling; }
+            set
+            {
+                _isSmiling = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _isMouthNormal = true;
+        public bool IsMouthNormal
+        {
+            get { return _isMouthNormal; }
+            set
+            {
+                _isMouthNormal = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _isTongueOut = false;
+        public bool IsTongueOut
+        {
+            get { return _isTongueOut; }
+            set
+            {
+                _isTongueOut = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public void SetExpressionData(PXCMFaceData.ExpressionsData expressionData)
+        {
+            if (expressionData != null)
+            {
+                IsFaceVisible = true;
+                foreach (var expression in Expressions)
+                {
+                    expression.CheckExpression(expressionData);
+                }
+            }
+            else
+                IsFaceVisible = false;
+        }
+
+    }
+}
---
+++ FaceExpressionWPF/MainWindow.xaml.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFCore;
+
+namespace FaceExpressionWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private PXCMSenseManager SenseManager;
+
+        private PXCMFaceModule FaceModule;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BearFace = new FaceModel();
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region UI Properties
+        private WriteableBitmap _ImageRGB;
+        public WriteableBitmap ImageRGB
+        {
+            get { return _ImageRGB; }
+            set
+            {
+                _ImageRGB = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private FaceModel _BearFace;
+        public FaceModel BearFace
+        {
+            get { return _BearFace; }
+            set
+            {
+                _BearFace = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = this;
+
+            SenseManager = PXCMSenseManager.CreateInstance();
+
+            SenseManager.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_COLOR, 1280, 720);
+            SenseManager.EnableFace();
+
+            InitializeCamera();
+            ConfigurePollingTask();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (TaskCancellationTokenSource != null)
+                TaskCancellationTokenSource.Cancel();
+            SenseManager.Close();
+            SenseManager.Dispose();
+        }
+
+        #region Inizializzazione e acquisizione
+        private void InitializeCamera()
+        {
+            FaceModule = SenseManager.QueryFace();
+
+            var config = FaceModule.CreateActiveConfiguration();
+            config.detection.isEnabled = true;
+            config.landmarks.isEnabled = false;
+            config.pose.isEnabled = false;
+            config.strategy = PXCMFaceConfiguration.TrackingStrategyType.STRATEGY_FARTHEST_TO_CLOSEST;
+
+            PXCMFaceConfiguration.ExpressionsConfiguration exprConfig = config.QueryExpressions();
+            exprConfig.Enable();
+            exprConfig.EnableAllExpressions();
+
+            if (config.ApplyChanges().IsSuccessful())
+            {
+                if (SenseManager.Init().IsError())
+                {
+                    MessageBox.Show("Errore nell'inizializzazione della camera");
+                    Close();
+                }
+            }
+
+            config.Dispose();
+        }
+
+        private Task PollingTask;
+        private CancellationTokenSource TaskCancellationTokenSource;
+        private CancellationToken PollingTaskCancellationToken;
+
+        private void ConfigurePollingTask()
+        {
+            TaskCancellationTokenSource = new CancellationTokenSource();
+            PollingTaskCancellationToken = TaskCancellationTokenSource.Token;
+            PollingTask = new Task(PollingCode);
+            PollingTask.Start();
+        }
+
+        private void PollingCode()
+        {
+            PXCMFaceData faceData = FaceModule.CreateOutput();
+
+            while (!PollingTaskCancellationToken.IsCancellationRequested)
+            {
+                if (SenseManager.AcquireFrame().IsSuccessful())
+                {
+                    faceData.Update();
+                    var face = faceData.QueryFaceByIndex(0);
+                    var sample = SenseManager.QuerySample();
+                    ElaborateSample(sample, face);
+                    if (!PollingTaskCancellationToken.IsCancellationRequested) SenseManager.ReleaseFrame();
+                }
+            }
+        }
+        #endregion
+
+
+        private void ElaborateSample(PXCMCapture.Sample sample, PXCMFaceData.Face face)
+        {
+            if (sample == null) return;
+
+            WriteableBitmap imageRGB = null;
+
+            if (sample.color != null)
+            {
+                imageRGB = sample.color.GetImage();
+            }
+
+            if (imageRGB != null)
+                imageRGB.Freeze();
+
+            Dispatcher.Invoke(() =>
+            {
+                this.ImageRGB = imageRGB;
+                if (face != null)
+                    BearFace.SetExpressionData(face.QueryExpressions());
+                else
+                    BearFace.SetExpressionData(null);
+            });
+
+        }
+    }
+}
